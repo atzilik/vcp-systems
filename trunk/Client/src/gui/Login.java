8d1bddda211b25500d53a2ecaf0ad5f10b42e49a
galhalevi26@gmail.com <galhalevi26@gmail.com@b4a5d6fb-decf-e412-9ad5-2b6714ac1237>
2013-12-30 20:04:18 +0000

    sending and receiving a message example (i think...)

--- trunk/Client/src/gui/Login.java
+++ trunk/Client/src/gui/Login.java
@@ -1,61 +1,60 @@
 package gui;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
 import Messages.MessageLogin;
 
+import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-public class Login extends Frame{
+public class Login extends AbstractGUIComponent{
 	private JTextField userName;
 	private JPasswordField pass;
 	
 
 	public Login(){
-		super();
-		setTitle("Login");
+//		super();
+//		setTitle("Login");
 		JLabel lblUserName = new JLabel("User Name:");
 		lblUserName.setBounds(85, 69, 71, 14);
-		getContentPane().add(lblUserName);
+		add(lblUserName);
 
 		userName = new JTextField();
 		userName.setBounds(210, 66, 86, 20);
-		getContentPane().add(userName);
+		add(userName);
 		userName.setColumns(10);
 
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setBounds(85, 136, 71, 14);
-		getContentPane().add(lblPassword);
+		add(lblPassword);
 
 		pass = new JPasswordField();
 		pass.setBounds(210, 133, 86, 20);
-		getContentPane().add(pass);
+		add(pass);
 		pass.setColumns(10);
 
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					getClient().sendToServer(new MessageLogin(userName.getText(), new String(pass.getPassword())));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+				client.send(new MessageLogin(userName.getText(), new String(pass.getPassword())));
+				MessageLogin ml = (MessageLogin)client.getMessage();
+				if (ml.isEmpty())
+				//no such user
+				{
+					JOptionPane.showMessageDialog(null, "Wrong username or password.", "Error", JOptionPane.ERROR_MESSAGE);
 				}
-//				String[] arr = new String[3];
-//				arr[0] = "check login";
-//				arr[1] = userName.getText();
-//				arr[2] = new String(pass.getPassword());
-//				try {
-//					getC().sendToServer(arr);
-//				} catch (IOException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
+				else
+				//TODO 
+				{
+					
+				}
+
 //				new Thread(new Runnable() {
 //					
 //					@Override
@@ -77,7 +76,7 @@ public class Login extends Frame{
 			}
 		});
 		btnSubmit.setBounds(104, 209, 89, 23);
-		getContentPane().add(btnSubmit);
+		add(btnSubmit);
 
 		JButton btnClear = new JButton("Clear");
 		btnClear.addActionListener(new ActionListener() {
@@ -87,7 +86,7 @@ public class Login extends Frame{
 			}
 		});
 		btnClear.setBounds(272, 209, 89, 23);
-		getContentPane().add(btnClear);
+		add(btnClear);
 
 	}
 
