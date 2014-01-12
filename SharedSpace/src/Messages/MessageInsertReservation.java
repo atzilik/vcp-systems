505552a2bf112f149093c30b567757133f4b0bb0
galhalevi26 <galhalevi26@gmail.com>
2014-01-12 17:44:49 +0000

    added date for reservation

--- SharedSpace/src/Messages/MessageInsertReservation.java
+++ SharedSpace/src/Messages/MessageInsertReservation.java
@@ -26,7 +26,7 @@ public class MessageInsertReservation extends Message {
 		
 		try {
 			//insert reservation to reservations table
-			PreparedStatement ps = con.prepareStatement("INSERT INTO reservations (reservationId,carId,customerId,parkingLotId,estCinDate,estCinHour,estCotDate,estCotHour,inAdvance) VALUES(?,?,?,?,?,?,?,?,?);");
+			PreparedStatement ps = con.prepareStatement("INSERT INTO reservations (reservationId,carId,customerId,parkingLotId,estCinDate,estCinHour,estCotDate,estCotHour,inAdvance,estBill,reservationDate) VALUES(?,?,?,?,?,?,?,?,?,?,?);");
 			for (int i = 0 ; i < details.length - 1; i++)
 			{
 				ps.setString(i + 1, details[i]);
@@ -39,29 +39,23 @@ public class MessageInsertReservation extends Message {
 			{
 				ps.setBoolean(9, false);
 			}
-			ps.executeUpdate();
-			ps.close();
+			
 			
 			double bill;
 			//getting the parking lot rates in order to calculate estimated bill
-			ps = con.prepareStatement("SELECT * FROM parkinglots WHERE parkingLotID=?;");
-			ps.setString(1, details[3]);
-			ResultSet rs = ps.executeQuery();
+			PreparedStatement ps1 = con.prepareStatement("SELECT * FROM parkinglots WHERE parkingLotID=?;");
+			ps1.setString(1, details[3]);
+			ResultSet rs = ps1.executeQuery();
 			if (rs.next())
 			{
 				double occRate = rs.getDouble(7);
 				double resRate = rs.getDouble(8);
-				ps.close();
+				ps1.close();
 				//getting the dates in order to calculate bill
-				ps = con.prepareStatement("SELECT * FROM reservations WHERE reservationId=?;");
-				ps.setString(1, details[0]);
-				rs = ps.executeQuery();
-				rs.next();
-				Date cinDate = rs.getDate(5);
-				Time cinTime = rs.getTime(6);
-				Date cotDate = rs.getDate(7);
-				Time cotTime = rs.getTime(8);
-				ps.close();
+				Date cinDate = DateConvert.stringToSQLDate(details[4]);
+				Time cinTime = DateConvert.stringToSQLTime(details[5]);
+				Date cotDate = DateConvert.stringToSQLDate(details[6]);
+				Time cotTime = DateConvert.stringToSQLTime(details[7]);
 				// if inAdvance = 1 then its reservation from advance
 				if (details[8].equals("1"))
 				{
@@ -73,14 +67,13 @@ public class MessageInsertReservation extends Message {
 				{//wrong data if occasional customer put an early hour then the current hour
 					bill = DateConvert.timeDifference(DateConvert.buildFullDate(cotDate,cotTime),DateConvert.buildFullDate(cinDate,cinTime)) * (occRate / 60);
 				}
-				ps = con.prepareStatement("UPDATE reservations SET estBill=? WHERE reservationId=?;");
-				ps.setDouble(1, bill);
-				ps.setString(2, details[0]);
+				ps.setDouble(10, bill);
+				ps.setDate(11, DateConvert.getCurrentSqlDate());
 				ps.executeUpdate();
-				ps.close();
 			}
 			else
 			{
+				ps1.close();
 				return new MessageInsertReservationReply();
 			}
 			this.setTransactionSucceeded(true);	
