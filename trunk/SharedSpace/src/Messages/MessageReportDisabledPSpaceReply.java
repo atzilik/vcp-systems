bba89c91f9503e83a75206f07e1cd12d62501060
atzilik@gmail.com <atzilik@gmail.com@b4a5d6fb-decf-e412-9ad5-2b6714ac1237>
2014-01-12 17:41:12 +0000

    Updated ReportDisabledParkingSpace to allow various disabled parking spaces reports production

--- trunk/SharedSpace/src/Messages/MessageReportDisabledPSpaceReply.java
+++ trunk/SharedSpace/src/Messages/MessageReportDisabledPSpaceReply.java
@@ -3,11 +3,21 @@ package Messages;
 import javax.swing.JOptionPane;
 
 public class MessageReportDisabledPSpaceReply extends Message {
-
+boolean disabled;
+	public MessageReportDisabledPSpaceReply(boolean disabled) {
+	this.disabled = disabled;
+}
 	@Override
 	public Message doAction() {
 		// TODO Auto-generated method stub
+		if (disabled==true){
 		JOptionPane.showMessageDialog(null, "ParkingSpace is now disabled...");
+		}
+		else
+		{
+		JOptionPane.showMessageDialog(null, "ParkingSpace is now enabled...");
+
+		}
 		return null;
 	}
 
